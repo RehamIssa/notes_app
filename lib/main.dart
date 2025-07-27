7a3e0ae4498c5b96bb2eb7f4e76b9291b267c4fe
RehamIssa <reham.eissa1234@gmail.com>
2025-07-28 02:15:12 +0300

    Changing the theme to dark mode and creating home view.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //Changing the app to dark mode
+      theme: ThemeData(brightness: Brightness.dark),
+      home: HomeView(),
+    );
+  }
+}
