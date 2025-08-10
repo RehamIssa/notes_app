71fcc60a259527012f061306a996a9c8048c4529
RehamIssa <reham.eissa1234@gmail.com>
2025-08-10 03:53:16 +0300

    create edit view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
@@ -7,52 +7,64 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Don't give the conatiner any fixed height
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.orangeAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.all(0),
-            title: const Text('Flutter Tips',
-                style: TextStyle(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.orangeAccent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.all(0),
+              title: const Text('Flutter Tips',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                  )),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: const Text(
+                  'You can reach your goals by daily practice',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
                   color: Colors.black,
-                  fontSize: 24,
-                )),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 12.0),
-              child: const Text(
-                'You can reach your goals by daily practice',
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 18,
+                  size: 32,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 32,
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+                top: 16,
+              ),
+              child: const Text(
+                'Augst7, 2025',
+                style: TextStyle(color: Colors.black54),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 16,
-              top: 16,
-            ),
-            child: const Text(
-              'Augst7, 2025',
-              style: TextStyle(color: Colors.black54),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
