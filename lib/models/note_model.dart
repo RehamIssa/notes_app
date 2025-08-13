9706cb8649ee45ed3787052b6fd2b6ac8c8d0db7
RehamIssa <reham.eissa1234@gmail.com>
2025-08-13 10:22:02 +0300

    generating the type adaptor code for hive package

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,24 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+/* 
+To generate a type adaptor
+1- put part 'note_model.g.dart'; which include the name of the current file
+2-add @HiveType(typeId:0) for the class
+3-add @HiveField(index) for each attribute in the class
+ and each one should have a unique value
+ 4- open the terminal and run this command: 
+ flutter packages pub run build_runner build
+ */
+
+@HiveType(typeId: 0) //put any number from 0 to 255
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel({
     required this.title,
