13d0627842edd8c9f20ebf2b555729c086be4411
RehamIssa <reham.eissa1234@gmail.com>
2025-09-15 23:51:01 +0300

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/home_view.dart';
 
 void main() async {
- await Hive.initFlutter(); //focus on using initFlutter b/c we are working with flutter
- await Hive.openBox(kNotesBox); //the box is something like a collection to save the data in it
+  await Hive
+      .initFlutter(); //focus on using initFlutter b/c we are working with flutter
+  await Hive.openBox(
+      kNotesBox); //the box is something like a collection to save the data in it
+  Hive.registerAdapter(
+      NoteModelAdapter()); //to tell hive that the NoteModel is the one tp be saved in hive
   runApp(const NotesApp());
 }
 
